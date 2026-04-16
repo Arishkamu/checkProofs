9d5aceef2e7ab7de42817c88e4ac660c65bf7736
Arina Ivanova <arishkamu@gmail.com>
2026-04-16 13:15:27 +0300

    Just Parse to ast

--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,121 @@
-{-# LANGUAGE TemplateHaskell #-}
-module Main where
+{-# LANGUAGE LambdaCase #-}
 
---import Language.Haskell.Exts.Parser
---import Language.Haskell.Exts.ExactPrint
---import Language.Haskell.Exts.Syntax
---import Language.Haskell.Exts.SrcLoc
---import Language.Haskell.Exts.Pretty
---import Language.Haskell.Exts.Comments
---import Language.Haskell.Exts
+import GHC
+import GHC.Paths (libdir)
+import GHC.Driver.Flags
+import GHC.Utils.Outputable
+import GHC.Core
 
-import Language.Haskell.TH.Syntax
+import Control.Monad.IO.Class
 
 import System.Directory (getCurrentDirectory)
 import System.FilePath ((</>))
 
-import CommentParser (cmtParsed)
-import Extra
-
-
--- $(myAST)
+import Data.List (intercalate)
 
 main :: IO ()
-main = do
-    dir <- getCurrentDirectory
-    src <- readFile $ dir </> "test" </> "BasicTest.hs"
-    putStrLn $ cmtParsed src
-    expr1 <- runQ $ myAST1
-    print $ expr1
-    expr2 <- runQ $ myAST2
-    print $ expr2
---    let code1 = "f . (g . h)"
---    let code2 = "\\x -> f ((g . h) x)"
---    putStrLn "================="
---    case parseExp code1 of
---        ParseOk ast -> print ast
---        ParseFailed loc msg -> putStrLn $ "Parse error at " ++ show loc ++ ": " ++ msg
---    putStrLn "====="
---    case parseExp code2 of
---        ParseOk ast -> print ast
---        ParseFailed loc msg -> putStrLn $ "Parse error at " ++ show loc ++ ": " ++ msg
+main =
+  runGhc (Just libdir) $ do
+    dflags <- getSessionDynFlags
+    _ <- setSessionDynFlags dflags
+
+    -- load the file
+--    let path = "/Users/arina/hse/nir/moskvinPrj/checkProofs/test/BasicTest.hs"
+    let path = "/Users/arina/hse/nir/moskvinPrj/checkProofs/old/Example.hs"
+    target <- guessTarget path Nothing Nothing
+    setTargets [target]
+
+    -- parse it
+    modGraph <- depanal [] False
+    let ms = head $ mgModSummaries modGraph
+--
+--    parsed <- parseModule ms
+--
+--    liftIO $ putStrLn "=== Parsed AST ==="
+--    liftIO $ putStrLn (showSDocUnsafe (ppr (pm_parsed_source parsed)))
+--
+--    typed <- typecheckModule parsed
+--
+--    liftIO $ putStrLn "=== Renamed AST ==="
+--    liftIO $ putStrLn (showSDocUnsafe (ppr (tm_renamed_source typed)))
+--
+--    liftIO $ putStrLn "=== Typechecked AST ==="
+--    liftIO $ putStrLn (showSDocUnsafe (ppr (tm_typechecked_source typed)))
+--
+--    desugared <- desugarModule typed
+--
+--    core <- compileToCoreModule path
+--
+--    liftIO $ putStrLn "=== Core AST ==="
+--    liftIO $ putStrLn (showSDocUnsafe (ppr $ cm_binds core))
+--
+--    let bindGlExprs = map goFunc (cm_binds core)
+--    liftIO $ putStrLn $ intercalate "\n" bindGlExprs
+--    mapM_ (\ex -> liftIO (putStrLn ("=======\n" ++ (showSDocUnsafe (ppr ex))))) bindGlExprs
+
+    case mgModSummaries modGraph of
+      [] -> liftIO $ putStrLn "No module found"
+      (ms:_) -> do
+        p <- parseModule ms
+        let parsed = pm_parsed_source p
+        liftIO $ putStrLn $ analyzeModule parsed
+        liftIO $ putStrLn (showSDocUnsafe (ppr parsed))
+
+
+
+--analyzeModule :: ParsedSource -> String
+--analyzeModule (L _ modu) =
+--  case hsmodDecls modu of
+--    decls -> concatMap analyzeDecl decls
+
+analyzeModule :: ParsedSource -> String
+analyzeModule (L _ modu) =
+  case hsmodDecls modu of
+    decls -> concatMap analyzeDecl decls
+
+analyzeDecl :: LHsDecl GhcPs -> String
+analyzeDecl (L _ decl) =
+  case decl of
+    ValD _ bind -> analyzeBind bind
+    TyClD _ t   -> "Type\n"
+    _           -> "AnyDecl\n"
+
+--analyzeBind :: HsBind GhcPs -> String
+--analyzeBind bind =
+--  case bind of
+--    FunBind _ _ _ -> "FunBind\n"
+--    _           -> "AnyBind\n"
+analyzeBind :: HsBind GhcPs -> String
+analyzeBind bind =
+  case bind of
+    FunBind { fun_matches = mg } -> analyzeMatchGroup mg
+    PatBind {} -> "Pattern binding"
+    _ -> "AnyBind"
+
+analyzeMatchGroup :: MatchGroup GhcPs (LHsExpr GhcPs) -> String
+analyzeMatchGroup mg = concatMap analyzeMatch (unLoc (mg_alts mg))
+
+analyzeMatch :: LMatch GhcPs (LHsExpr GhcPs) -> String
+analyzeMatch (L _ match) = analyzeGRHSs (m_grhss match)
+
+analyzeGRHSs :: GRHSs GhcPs (LHsExpr GhcPs) -> String
+analyzeGRHSs grhss = concatMap analyzeGRHS (grhssGRHSs grhss)
+
+analyzeGRHS :: LGRHS GhcPs (LHsExpr GhcPs) -> String
+analyzeGRHS (L _ (GRHS _ _ body)) = analyzeExpr body
+
+analyzeExpr :: LHsExpr GhcPs -> String
+analyzeExpr (L _ expr) =
+    case expr of
+        OpApp _ l op r -> "Operator application: {\n" ++ "OP  " ++ analyzeExpr op ++ "\n  " ++ analyzeExpr l ++ "\n  " ++ analyzeExpr r ++ "}\n"
+        _ -> (showSDocUnsafe (ppr expr))
+--  case expr of
+--    HsApp _ f arg -> "Application: {\n" ++ analyzeExpr f ++ "\n" ++ analyzeExpr arg ++ "}\n"
+--    OpApp _ l op r -> "Operator application: {\n" ++ analyzeExpr l ++ "\n" ++ analyzeExpr r ++ "}\n"
+--    HsLam _ _ mg -> "Lambda: {\n" ++ analyzeMatchGroup mg  ++ "}\n"
+--    HsLet _ _ body -> analyzeExpr body
+--    HsIf _ a b c -> analyzeExpr a ++ analyzeExpr b ++ analyzeExpr c
+--    HsVar _ name -> "Var: " ++ (showSDocUnsafe (ppr name))
+--    HsLit _ lit -> "Literal: " ++ (showSDocUnsafe (ppr lit))
+--    _ -> "AnyExpr\n"
+
--- test/BasicTest.hs
+++ test/BasicTest.hs
@@ -1,3 +1,4 @@
+-- Ver. 1
 infixl 0 ===
 (===) :: a -> a -> a
 (===) = const
@@ -36,8 +37,52 @@ composeAssoc f g h =
  === (\x -> (f . g) (h x))           -- (.)
  ===  (f . g) . h
 
-theoremApplicative3 :: Monad m => m (a -> b) -> a -> m b
-theoremApplicative3 fs x =
-  fs <*>.. return x  ===         -- [lemma1]
-  fs >>= \f -> return (f x) ===  -- [lemma2]
-  return ($ x) <*>.. fs
+-- Ver. 2
+infixl 0 =====
+(=====) :: (a -> String) -> (a -> String) -> (a -> String)
+(=====) x y = y
+
+infixl 0 ======
+(======) :: (a -> String) -> a -> a
+(======) x y = y
+
+
+composeAssoc2 :: (c -> d) -> (b -> c) -> (a -> b) -> a -> (d -> String)
+composeAssoc2 f g h =
+     (f . (g . h)) "(.)"
+ ===== ((\x -> f ((g . h) x))) "(.)"
+ ===== ((\x -> f ((\x' -> g (h x')) x))) "{beta}"
+ ===== ((\x -> f (g (h x)))) "{beta привет}"
+ ===== ((\x -> (\x' -> f (g x')) (h x))) "(.)"
+ ===== ((\x -> (f . g) (h x))) "(.)"
+ ====== ((f . g) . h)
+
+
+
+-- Ver. 3
+data WithInfo a = WithInfo { value :: a, info :: String }
+
+infixl 0 ====
+(====) :: WithInfo a -> WithInfo a -> WithInfo a
+(====) x y = y
+
+
+composeAssoc2 :: (c -> d) -> (b -> c) -> (a -> b) -> a -> d
+composeAssoc2 f g h =
+     WithInfo (f . (g . h)) "(.)"
+ ==== WithInfo ((\x -> f ((g . h) x))) "(.)"
+ ==== WithInfo ((\x -> f ((\x' -> g (h x')) x))) "{beta}"
+ ==== WithInfo ((\x -> f (g (h x)))) "{beta привет}"
+ ==== WithInfo ((\x -> (\x' -> f (g x')) (h x))) "(.)"
+ ==== WithInfo ((\x -> (f . g) (h x))) "(.)"
+ ==== value $ WithInfo ((f . g) . h) "(.)"
+
+--theoremApplicative3 :: Monad m => m (a -> b) -> a -> m b
+--theoremApplicative3 fs x =
+--  fs <*>.. return x  ===         -- [lemma1]
+--  fs >>= \f -> return (f x) ===  -- [lemma2]
+--  return ($ x) <*>.. fs
+
+res1 f g h = (\x -> f ((g . h) x)) where (.) f g = \x -> f (g x)
+
+res2 f g h = f . (g . h) where (.) f g = \x -> f (g x)
