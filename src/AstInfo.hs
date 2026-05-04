b5e3009f5d018b23d4521b6b713b0d27321c13dc
Arina Ivanova <arishkamu@gmail.com>
2026-05-04 19:26:33 +0300

    upd ast info

--- src/AstInfo.hs
+++ src/AstInfo.hs
@@ -1,7 +1,10 @@
 module AstInfo where
 
-import GHC.Core ( CoreExpr )
+import GHC.Core ( CoreExpr, CoreBndr )
 import GHC.Types.Id ( Id )
+
+import Control.Monad.Except (ExceptT)
+import Control.Monad.State.Lazy (StateT)
 
 -- data Conversion = Conversion {
 --   lhsCN     :: CoreExpr,
@@ -16,18 +19,99 @@ type SideExprInfo = Either ExprInfo ExprInfo
 data ExprInfo = Func String | Postl String | Beta | Eta
 
 data Conversion = Conversion {
-  cn_lhe  :: CoreExpr,
-  cn_rhe  :: CoreExpr,
+  cn_lhs  :: CoreExpr,
+  cn_rhs  :: CoreExpr,
   cn_info :: SideExprInfo
 }
 
 -- Add local Where to DeclConversions???
 type DeclConversions = (Id, [Conversion])
 type FuncDef  = (Id, CoreExpr)
-type PostlDef = (Id, CoreExpr, CoreExpr)
 
-data AstInfo = AstInfo {
-  ast_declconvrs   :: [DeclConversions], -- List of Conversion per decl
-  ast_funcdefs     :: [FuncDef], -- List of all function definitions (decls)
-  ast_postldefs    :: [PostlDef] -- List of all postulate definitions
+data PostlDef = PostlDef {
+  pstl_id     :: Id,
+  pstl_binds  :: [CoreBndr],
+  pstl_lhs    :: CoreExpr,
+  pstl_rhs    :: CoreExpr
 }
+
+-- data AstInfo = AstInfo {
+--   ast_declconvrs   :: [DeclConversions], -- List of Conversion per decl
+--   ast_funcdefs     :: [FuncDef], -- List of all function definitions (decls)
+--   ast_postldefs    :: [PostlDef] -- List of all postulate definitions
+-- }
+
+data CheckerST = CheckerST {
+  st_declconvrs   :: [DeclConversions], -- List of Conversion per decl
+  st_funcdefs     :: [FuncDef], -- List of all function definitions (decls)
+  st_postldefs    :: [PostlDef] -- List of all postulate definitions
+}
+
+type CheckerM = ExceptT String (StateT CheckerST IO)
+
+{-
+  I WANT 
+  to return REPORT:
+    Failure: 
+      [ 
+        in function--X
+        in step--Y
+        Reason
+      ]
+    Succses:
+      [Function]
+  
+  OR:
+    For each function have:
+      Failure:
+        in Step--Y
+        Reason
+      Succses
+  
+ExceptT e (StateT s IO)
+  1) Get List of functions
+  2) Separate them
+      Each of them or
+        should be proved (A)
+        is a postulate
+        Just function
+  3) go throught (A)
+    for each create report
+    update state if succses
+  
+  
+
+  [Func] [Postuls] (NTP : NeedsToProve) -> [Func] [Postuls, NTP] (NeedsToProve)
+  [Func]    = [Id, funcBody = (\x y z -> expr)]
+  [Postuls] = [Id, lhs, rhs, fn_binds]
+  NTP       = [Id, [(lhs, rhs)]]
+
+ 
+ 
+ 
+ FOR LATER:
+  Func to NTP => (need to rememebr list of all Funcs)
+    get list of defined in where -> [Funcs] -> ...
+      get list of comments
+        1) all used functions should be visiable
+        2) add to graph
+    get 
+    
+  NTP       = [Id, [(lhs, rhs)]]
+
+decide order:
+  list of 'all visiable theorems at this point'
+  -> list of 'all used theorems'
+  theorem11 = [theorem12]
+  theorem12 = [theorem11]
+    where
+      theorem21 = [theorem12, theorem22, theorem23, theorem31, theorem32]
+        where
+          theorem31 = [theorem12, theorem32]
+          theorem32 = [theorem12, theorem31]
+      theorem22 = [theorem12, theorem21, theorem23, theorem33, theorem34]
+        where
+          theorem33 = [theorem34]
+          theorem34 = [theorem33]
+      theorem23 = 
+-}
---
+++ src/PrettyString.hs
@@ -0,0 +1,147 @@
+{-# LANGUAGE RecordWildCards, FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+module PrettyString (PrettyString, prettyString, prettyStringBinds) where
+
+import GHC.Core
+import GHC.Types.Id
+import GHC.Utils.Outputable (Outputable, showSDocUnsafe, ppr)
+import GHC.Types.Name.Occurrence (occNameString)
+import GHC.Types.Name (getOccName)
+
+import Data.List (intercalate)
+import Data.Data (toConstr)
+
+import AstInfo
+
+class PrettyString a where
+  prettyStringIdent :: Int -> a -> String
+--   prettyStringIdent ident x = showSDocUnsafe (ppr x)
+
+prettyString :: (PrettyString a) => a -> String
+prettyString = prettyStringIdent 0
+
+bslN :: Int -> String
+bslN ident = "\n" ++ replicate ident ' '
+
+prettyStringStrs :: Int -> [String] -> String
+prettyStringStrs ident = intercalate (bslN ident)
+
+-- instance (PrettyString a) => PrettyString (GenLocated SrcSpanAnnA a) where
+--   rettyString (L _ x) = rettyString x
+
+-- instance (Outputable a) => PrettyString Id where
+--   prettyStringIdent _ x = "\n" ++ replicate ident ' ' ++ showSDocUnsafe (ppr x)
+
+instance PrettyString Id where
+  prettyStringIdent _ x = "ID: " ++ occNameString (getOccName x)
+
+-- instance PrettyString (GenLocated SrcSpanAnnN Id) where
+--   rettyString (L _ x) = rettyString x
+
+instance PrettyString ExprInfo where
+    prettyStringIdent _ expr_info = case expr_info of
+        Func  comment -> "Func: "  ++ comment
+        Postl comment -> "Postl: " ++ comment
+        Beta          -> "Beta reduction"
+        Eta           -> "Eta reduction"
+
+instance PrettyString Conversion where
+  prettyStringIdent ident Conversion{..} = 
+    prettyStringIdent 0 cn_info ++ " :: "
+    ++ bslN (ident + 2) ++ "l: " ++ prettyStringIdent 0 cn_lhs
+    ++ bslN (ident + 2) ++ "r: " ++ prettyStringIdent 0 cn_rhs
+    -- ++ showSDocUnsafe (ppr cn_lhe) ++ " => " ++ showSDocUnsafe (ppr cn_rhe) 
+    -- ++
+    -- bslN ident       ++ "Different constructors: " ++ show (toConstr lDiffCN) ++ " vs " ++ show (toConstr rDiffCN) ++
+    -- bslN (ident + 2) ++ "l: " ++ rettyString lDiffCN ++ 
+    -- bslN (ident + 2) ++ "r: " ++ rettyString rDiffCN
+
+instance PrettyString PostlDef where
+  prettyStringIdent ident PostlDef{..} = 
+    bslN ident ++ "Postl_ID :: " ++ prettyStringIdent 0 pstl_id ++
+    bslN ident ++ "Binds:" ++ prettyStringIdent (ident + 2) pstl_binds ++
+    bslN (ident + 2) ++ "l_pstl: " ++ prettyStringIdent 0 pstl_lhs ++
+    bslN (ident + 2) ++ "r_pstl: " ++ prettyStringIdent 0 pstl_rhs
+
+instance PrettyString CheckerST where
+  prettyStringIdent :: Int -> CheckerST -> String
+  prettyStringIdent ident CheckerST{..} =
+    "===== AstInfo =====" 
+    ++ bslN ident ++ "ST_declconvrs:"  ++ prettyStringStrs  (ident + 2) (concatMap makePretty st_declconvrs)  
+    ++ bslN ident ++ "ST_funcDefs:"    ++ prettyStringIdent (ident + 2) st_funcdefs
+    ++ bslN ident ++ "ST_postlDefs:"   ++ prettyStringIdent (ident + 2) st_postldefs
+
+    where
+    makePretty (decl, convrs) =
+        (bslN (ident + 2) ++ "DeclName: " ++  prettyStringIdent (ident + 2) decl) 
+        : map (prettyStringIdent (ident + 4)) convrs
+
+-- -- Represent: LHsBindLR GhcTc
+-- --instance PrettyString (HsBindLR GhcTc GhcTc) where
+-- --  prettyString (L _ bind) = prettyString bind
+
+prettyStringBinds :: [FuncDef] -> String
+prettyStringBinds binds = intercalate (bslN 2) $ map prettyStringBind binds
+  where
+    prettyStringBind (fnId, fnBody) = "Function: " ++ prettyString fnId ++ "\nBody: " ++ prettyStringExpr fnBody
+
+prettyStringExpr :: CoreExpr -> String
+prettyStringExpr expr = case expr of
+  Lam args body -> "Lam: " ++ "\n    " ++ showSDocUnsafe (ppr args) ++ "\n" ++ prettyStringExpr body
+  App f arg -> "App: " ++ prettyStringExpr f ++ "\n to " ++ prettyStringExpr arg
+  Var v -> "Var: " ++ showSDocUnsafe (ppr v) 
+  _ -> "NotImpl: " ++ showSDocUnsafe (ppr expr)
+
+instance PrettyString CoreBind where
+  prettyStringIdent ident bind = prettyStringIdent ident $ flattenBinds [bind]
+-- instance PrettyString CoreBind where
+--     prettyStringIdent ident (NonRec v expr) =
+--       "NonRec: " ++ prettyStringIdent (ident + 2) v 
+--     --   ++ "\n" ++ prettyStringIdent (ident + 2) expr
+--     prettyStringIdent ident (Rec vExprs) =
+--       "Rec: " ++ intercalate (bslN (ident + 2)) (map (\(v, e) -> prettyStringIdent (ident + 4) v {- ++ "\n" ++ prettyStringIdent (ident + 4) e -}) vExprs)
+
+-- instance PrettyString (HsBindLR GhcTc GhcTc) where
+--   prettyString bind =
+-- --  show (toConstr bind) ++ " :: " ++ (showSDocUnsafe (ppr bind))
+--     case bind of
+--       fb@FunBind{ fun_matches = mg } -> "FUNBIND: " ++ "\nID:" ++ showSDocUnsafe (ppr (unLoc (fun_id fb))) ++ "\n" ++ (showSDocUnsafe (ppr bind))
+-- --      analyzeMatchGroup mg
+-- --          PatBind{} -> []
+-- --          VarBind{} ->
+-- --          PatSynBind{} -> "PatSynBind"
+--       (XHsBindsLR a) -> "XHsBindsLR" ++ " :: " ++ prettyString (abs_binds a)
+--       _ -> show (toConstr bind) ++ " :: " ++ (showSDocUnsafe (ppr bind))
+
+instance PrettyString CoreExpr where
+--   prettyStringIdent _ (App f arg) = "APP " ++ " :: " ++ "\nF: " ++ (showSDocUnsafe (ppr f)) ++ "\nA: " ++ (showSDocUnsafe (ppr arg))
+  prettyStringIdent _ expr = show (toConstr expr) ++ " :: " ++ (showSDocUnsafe (ppr expr))
+
+instance (PrettyString a, PrettyString b) => PrettyString (a, b) where
+  prettyStringIdent ident (x, y) = bslN ident ++ "(" 
+    ++ "\n" ++ prettyStringIdent (ident + 2) x 
+    ++ "\n" ++ prettyStringIdent (ident + 2) y
+    ++ bslN ident ++ ")"
+
+instance (PrettyString a, PrettyString b, PrettyString c) => PrettyString (a, b, c) where
+  prettyStringIdent ident (x, y, z) = bslN ident ++ "(" 
+    ++ "\n" ++ prettyStringIdent (ident + 2) x 
+    ++ "\n" ++ prettyStringIdent (ident + 2) y 
+    ++ "\n" ++ prettyStringIdent (ident + 2) z 
+    ++ bslN ident ++ ")"
+
+
+instance (PrettyString a) => PrettyString [a] where
+  prettyStringIdent ident as = intercalate (bslN ident) $ map (prettyStringIdent (ident + 2)) as
+
+instance (PrettyString a) => PrettyString (Either String a) where
+  prettyStringIdent ident (Left e)  = "Either-Left:"  ++ bslN (ident + 2) ++ e
+  prettyStringIdent ident (Right a) = "Either-Right:" ++ bslN (ident + 2) ++ prettyString a
+
+instance (PrettyString a) => PrettyString (Either a a) where
+  prettyStringIdent ident (Left l)  = "L-" ++ prettyStringIdent ident l
+  prettyStringIdent ident (Right r) = "R-" ++ prettyStringIdent ident r
+
+
+-- instance (PrettyString a) => PrettyString (Bag a) where
+--   prettyString = prettyString . bagToList
