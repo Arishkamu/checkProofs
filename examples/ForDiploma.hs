32a46e0106f3246f272db451309064eaaefe697d
Arina Ivanova <arishkamu@gmail.com>
2026-05-21 13:45:28 +0300

    del unused files

--- examples/ForDiploma.hs
+++
@@ -1,7 +0,0 @@
-module ForDiploma where
-
-simpleFunction :: Show a => (a, String) -> String
-simpleFunction (n, todo) = show n ++ ": " ++ todo
-
--- simpleFunction :: a -> a
--- simpleFunction x = x
